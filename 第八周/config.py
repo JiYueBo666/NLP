ac8692d12d89ab5d96806b9277b8c4c03e9be2c7
JiYueBo666 <121330570+JiYueBo666@users.noreply.github.com>
2024-03-07 20:47:41 +0800

    Update config.py

--- 第八周/config.py
+++ 第八周/config.py
@@ -7,9 +7,9 @@ config={
     "max_len":30,
     "embedding_dim":128,
     'hidden_dim':128,
-    "train_path":r"E:\badouFile\第八周\week8 文本匹配问题\week8 文本匹配问题\data\train.json",
-    "valid_path":r"E:\badouFile\第八周\week8 文本匹配问题\week8 文本匹配问题\data\valid.json",
-    "schema_path":r"E:\badouFile\第八周\week8 文本匹配问题\week8 文本匹配问题\data\schema.json",
+    "train_path":r"E:data\train.json",
+    "valid_path":r"E:data\valid.json",
+    "schema_path":r"E:data\schema.json",
     "positive_rate":0.5,#正负样本比例
     "epoch_data_size": 1000,  # 每轮训练中采样数量
     "out_dim":128,
